0f33dbfea05a771e8ac8e3a043c2963dd9138f51
Tiny0pus <143680478+Tiny0pus@users.noreply.github.com>
2025-02-24 09:22:10 -0700

    runner

---
+++ src/controller/Controller.java
@@ -0,0 +1,31 @@
+package controller;
+
+public class Controller
+{
+	// Zero param constructor for Controller
+	public Controller()
+	{
+		
+	}
+	
+	//Start the server
+	//This method should do many things such as
+	//Initiate a connection to a database
+	//Prepare itself to connect to clients
+	//Connect to peripheral devices
+	
+	public void start()
+	{
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
+
+
---
+++ src/controller/Runner.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class Runner
+{
+	//Main method to start the program
+	public static void main(String[] args)
+	{
+		Controller app = new Controller();
+		app.start();
+
+	}
+
+}
